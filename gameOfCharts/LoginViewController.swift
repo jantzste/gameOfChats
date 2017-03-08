6eddfeb6fc96132c767433a8e460b898bade8760
Stephen Jantz <sjantz@hotmail.de>
2017-03-08 16:59:58 +0100

    change the color and the size of the Register button

--- gameOfCharts/LoginViewController.swift
+++ gameOfCharts/LoginViewController.swift
@@ -27,6 +27,8 @@ class LoginViewController: UIViewController {
         button.backgroundColor = UIColor(r:80, g: 101, b: 161)
         button.setTitle("Register", for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
         return button
     }()
     
@@ -59,7 +61,7 @@ class LoginViewController: UIViewController {
         loginRegisterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         loginRegisterButton.topAnchor.constraint(equalTo: inputsContainerView.bottomAnchor, constant: 12).isActive = true
         loginRegisterButton.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
-        loginRegisterButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        loginRegisterButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
         
     }
