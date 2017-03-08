2358ee0d89ade58f48e44e7baf8bf3be2ffc5140
Stephen Jantz <sjantz@hotmail.de>
2017-03-08 16:54:09 +0100

    add Register button and add the constraints programmatically

--- gameOfCharts/LoginViewController.swift
+++ gameOfCharts/LoginViewController.swift
@@ -22,7 +22,13 @@ class LoginViewController: UIViewController {
         return view
     }()
 
-    
+    let loginRegisterButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(r:80, g: 101, b: 161)
+        button.setTitle("Register", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
 
     override func viewDidLoad() {
@@ -31,8 +37,10 @@ class LoginViewController: UIViewController {
             //add the background color of the view
         view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
         view.addSubview(inputsContainerView)
+        view.addSubview(loginRegisterButton)
 
         setupInputsContainerView()
+        setupLoginRegisterButton()
     }
     
     
@@ -43,6 +51,16 @@ class LoginViewController: UIViewController {
         inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
         inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+    }
+    
+    func setupLoginRegisterButton(){
+    //need x, y width, height constraints
+        loginRegisterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginRegisterButton.topAnchor.constraint(equalTo: inputsContainerView.bottomAnchor, constant: 12).isActive = true
+        loginRegisterButton.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        loginRegisterButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
         
     }
     
